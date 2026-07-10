7248daa22cb091ffbe9d81c321790edf31a5c392
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2026-07-10 11:03:43 +0700

    Define API care plan service

---
+++ backend/src/main/java/com/eldercare/modules/careplan_management/careplan_design/service/CarePlanServiceImpl.java
@@ -0,0 +1,5 @@
+package com.eldercare.modules.careplan_management.careplan_design.service;
+
+public class CarePlanServiceImpl {
+
+}
---
+++ backend/src/main/java/com/eldercare/modules/careplan_management/careplan_design/service/ICarePlanService.java
@@ -0,0 +1,5 @@
+package com.eldercare.modules.careplan_management.careplan_design.service;
+
+public interface ICarePlanService {
+    public void activateCarePlan();
+}
