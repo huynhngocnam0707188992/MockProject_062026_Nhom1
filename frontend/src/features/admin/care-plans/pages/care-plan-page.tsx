093db4e0e975a9f4f3ed57462d33bdcea1fc5c24
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2026-07-10 10:54:41 +0700

    Add care plan page

--- frontend/src/features/admin/care-plans/pages/care-plan-page.tsx
+++ frontend/src/features/admin/care-plans/pages/care-plan-page.tsx
@@ -1,5 +1,17 @@
+import CarePlanOptions from "../components/care-plan-options";
+import CarePlanTitle from "../components/care-plan-title";
+
+import CarePlanStatistical from "../components/care-plan-statistical";
+import CarePlanTable from "../components/care-plan-table";
 const CarePlanPage = () => {
-  return <div>CarePlanPage</div>;
+  return (
+    <div>
+      <CarePlanTitle></CarePlanTitle>
+      <CarePlanOptions></CarePlanOptions>
+      <CarePlanStatistical></CarePlanStatistical>
+      <CarePlanTable></CarePlanTable>
+    </div>
+  );
 };
 
 export default CarePlanPage;
