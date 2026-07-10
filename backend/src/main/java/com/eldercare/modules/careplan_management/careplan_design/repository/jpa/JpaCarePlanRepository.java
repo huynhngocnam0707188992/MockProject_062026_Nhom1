4fc02d17363018d75b818cd7a7abf7ad7b062a97
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2026-07-10 15:21:18 +0700

    create interface jpa care plan repository

---
+++ backend/src/main/java/com/eldercare/modules/careplan_management/careplan_design/repository/jpa/JpaCarePlanRepository.java
@@ -0,0 +1,10 @@
+package com.eldercare.modules.careplan_management.careplan_design.repository.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eldercare.modules.careplan_management.careplan_design.repository.database_schema.CarePlanSchema;
+
+public interface JpaCarePlanRepository extends JpaRepository<CarePlanSchema, Long>{
+    
+}
