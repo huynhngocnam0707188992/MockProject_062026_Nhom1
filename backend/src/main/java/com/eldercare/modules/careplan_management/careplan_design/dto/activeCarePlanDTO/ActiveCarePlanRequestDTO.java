f42878a5f8a15a9151b81780fc3555fcfe90c949
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2026-07-10 15:18:13 +0700

    create active care plan dtos

---
+++ backend/src/main/java/com/eldercare/modules/careplan_management/careplan_design/dto/activeCarePlanDTO/ActiveCarePlanRequestDTO.java
@@ -0,0 +1,5 @@
+package com.eldercare.modules.careplan_management.careplan_design.dto.activeCarePlanDTO;
+
+public class ActiveCarePlanRequestDTO {
+    public int CarePlanId;
+}
---
+++ backend/src/main/java/com/eldercare/modules/careplan_management/careplan_design/dto/activeCarePlanDTO/ActiveCarePlanResponseDTO.java
@@ -0,0 +1,7 @@
+package com.eldercare.modules.careplan_management.careplan_design.dto.activeCarePlanDTO;
+
+public class ActiveCarePlanResponseDTO {
+   public int id;
+   public String status;
+   public String updatedAt;
+}
