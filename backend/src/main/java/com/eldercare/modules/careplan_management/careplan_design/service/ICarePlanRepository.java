17c5e63f763e6a33770b972ed3f88019b9f98be5
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2026-07-11 10:07:35 +0700

    add updateOne and getAll

--- backend/src/main/java/com/eldercare/modules/careplan_management/careplan_design/service/ICarePlanRepository.java
+++ backend/src/main/java/com/eldercare/modules/careplan_management/careplan_design/service/ICarePlanRepository.java
@@ -1,9 +1,17 @@
 package com.eldercare.modules.careplan_management.careplan_design.service;
 
+import java.util.List;
+
+import com.eldercare.modules.careplan_management.careplan_design.dto.listCarePlansDTO.ListCarePlanRequestDTO;
 import com.eldercare.modules.careplan_management.careplan_design.entity.CarePlanEntity;
 
 public interface ICarePlanRepository {
 
     CarePlanEntity findById(int id);
+
+    void updateOne(CarePlanEntity carePlanEntity);
+
+    List<CarePlanEntity> getAll(ListCarePlanRequestDTO request);
+
 
 }
