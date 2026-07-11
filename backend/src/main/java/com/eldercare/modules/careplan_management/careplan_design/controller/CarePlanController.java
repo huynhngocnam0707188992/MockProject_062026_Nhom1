67fdc34a1931da22b77448e2eb66ef56a3cd7ffc
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2026-07-11 10:03:38 +0700

    add care plan controller

---
+++ backend/src/main/java/com/eldercare/modules/careplan_management/careplan_design/controller/CarePlanController.java
@@ -0,0 +1,62 @@
+package com.eldercare.modules.careplan_management.careplan_design.controller;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eldercare.common.enums.CarePlanStatusEnum;
+import com.eldercare.common.response.ApiResponse;
+import com.eldercare.modules.careplan_management.careplan_design.dto.activeCarePlanDTO.ActiveCarePlanRequestDTO;
+import com.eldercare.modules.careplan_management.careplan_design.dto.activeCarePlanDTO.ActiveCarePlanResponseDTO;
+import com.eldercare.modules.careplan_management.careplan_design.dto.discontinueCarePlanDTO.DiscontinueCarePlanRequestDTO;
+import com.eldercare.modules.careplan_management.careplan_design.dto.discontinueCarePlanDTO.DiscontinueCarePlanResponseDTO;
+import com.eldercare.modules.careplan_management.careplan_design.dto.listCarePlansDTO.ListCarePlanRequestDTO;
+import com.eldercare.modules.careplan_management.careplan_design.dto.listCarePlansDTO.ListCarePlanResponseDTO;
+import com.eldercare.modules.careplan_management.careplan_design.dto.markSignificantChangeDTO.MarkSignificantChangeResponseDTO;
+import com.eldercare.modules.careplan_management.careplan_design.dto.markSignificantChangeDTO.MarkSignificantChangeRequestDTO;
+import com.eldercare.modules.careplan_management.careplan_design.service.ICarePlanService;
+
+@RestController
+@RequestMapping("/care-plans")
+public class CarePlanController {
+    private final ICarePlanService carePlanService;
+
+    public CarePlanController(ICarePlanService carePlanService) {
+        this.carePlanService = carePlanService;
+    }
+
+    @PatchMapping("/{carePlanId}/activate")
+    public ResponseEntity<ApiResponse<ActiveCarePlanResponseDTO>> activateCarePlan(@PathVariable int carePlanId) {
+        ApiResponse<ActiveCarePlanResponseDTO> response = ApiResponse.success(this.carePlanService.activateCarePlan(new ActiveCarePlanRequestDTO(carePlanId)));
+        response.setMessage("Care plan " + carePlanId + "activated");
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/{carePlanId}/discontinue")
+    public ResponseEntity<ApiResponse<DiscontinueCarePlanResponseDTO>> discontinueCarePlan(@PathVariable int carePlanId) {
+        ApiResponse<DiscontinueCarePlanResponseDTO> response = ApiResponse.success(this.carePlanService.discontinueCarePlan(new DiscontinueCarePlanRequestDTO(carePlanId)));
+        response.setMessage("Care plan " + carePlanId + " deactivated");
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/{carePlanId}/significant-change")
+    public ResponseEntity<ApiResponse<MarkSignificantChangeResponseDTO>> markSignificantChange(@PathVariable int carePlanId) {
+        ApiResponse<MarkSignificantChangeResponseDTO> response = ApiResponse.success(this.carePlanService.markSignificantChange(new MarkSignificantChangeRequestDTO(carePlanId)));
+        response.setMessage("Significant change flag updated");
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<ApiResponse<ListCarePlanResponseDTO>> listCarePlans(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "20") Integer size, @RequestParam(required = false) Long residentId, @RequestParam(required = false) CarePlanStatusEnum status, @RequestParam(required = false) Boolean significantChangeFlag, @RequestParam(defaultValue = "updatedAt") String sortBy, @RequestParam(defaultValue = "DESC") Sort.Direction sortDir) {
+        ListCarePlanRequestDTO request = new ListCarePlanRequestDTO(page, size, residentId, status, significantChangeFlag, sortBy, sortDir);
+        ApiResponse<ListCarePlanResponseDTO> response = ApiResponse.success(this.carePlanService.listCarePlans(request));
+
+        return ResponseEntity.ok(response);
+    }
+}
