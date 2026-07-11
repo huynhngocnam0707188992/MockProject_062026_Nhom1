fb1e19e8576d240ac47120bef8577ddd743c684f
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2026-07-11 13:01:43 +0700

    refactor schema care plan

--- backend/src/main/java/com/eldercare/modules/careplan_management/careplan_design/repository/database_schema/CarePlanSchema.java
+++ backend/src/main/java/com/eldercare/modules/careplan_management/careplan_design/repository/database_schema/CarePlanSchema.java
@@ -1,54 +1,58 @@
 package com.eldercare.modules.careplan_management.careplan_design.repository.database_schema;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.eldercare.common.enums.CarePlanStatusEnum;
-
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "care_plans")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class CarePlanSchema {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long id;
+    private Long id;
 
     @Column(nullable = false, length = 20)
-    public String status;
+    private String status;
 
     @Column(name = "significant_change_flag", nullable = false)
-    public Boolean significantChangeFlag = false;
+    private Boolean significantChangeFlag = false;
 
     @Column(name = "resident_id", nullable = false)
-    public Long residentId;
+    private Long residentId;
 
     @Column(name = "is_deleted", nullable = false)
-    public Boolean isDeleted = false;
+    private Boolean isDeleted = false;
+
+    @OneToMany(mappedBy = "carePlan", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CareGoalSchema> listCareGoal = new ArrayList<>();
+
+    @OneToMany(mappedBy = "carePlan", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CareInterventionSchema> listCareIntervention = new ArrayList<>();
 
     @Column(name = "created_at", nullable = false)
-    public OffsetDateTime createdAt;
+    private OffsetDateTime createdAt;
 
     @Column(name = "updated_at", nullable = false)
-    public OffsetDateTime updatedAt;
-
-    public CarePlanSchema() {
-    }
-
-    public CarePlanSchema(Long id, String status, Boolean significantChangeFlag, Long residentId, Boolean isDeleted,
-            OffsetDateTime createdAt, OffsetDateTime updatedAt) {
-        this.id = id;
-        this.status = status;
-        this.significantChangeFlag = significantChangeFlag;
-        this.residentId = residentId;
-        this.isDeleted = isDeleted;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
+    private OffsetDateTime updatedAt;
 
 }
