6d0b397de4c60fd07799842ae11e71b26ed49723
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2026-07-11 10:03:53 +0700

    add active care plan dto

--- backend/src/main/java/com/eldercare/modules/careplan_management/careplan_design/dto/activeCarePlanDTO/ActiveCarePlanRequestDTO.java
+++ backend/src/main/java/com/eldercare/modules/careplan_management/careplan_design/dto/activeCarePlanDTO/ActiveCarePlanRequestDTO.java
@@ -1,5 +1,18 @@
 package com.eldercare.modules.careplan_management.careplan_design.dto.activeCarePlanDTO;
 
 public class ActiveCarePlanRequestDTO {
-    public int CarePlanId;
+    public int carePlanId;
+
+    public ActiveCarePlanRequestDTO(int carePlanId) {
+        this.carePlanId = carePlanId;
+    }
+
+    public int getCarePlanId() {
+        return carePlanId;
+    }
+
+    public void setCarePlanId(int carePlanId) {
+        this.carePlanId = carePlanId;
+    }
+
 }
--- backend/src/main/java/com/eldercare/modules/careplan_management/careplan_design/dto/activeCarePlanDTO/ActiveCarePlanResponseDTO.java
+++ backend/src/main/java/com/eldercare/modules/careplan_management/careplan_design/dto/activeCarePlanDTO/ActiveCarePlanResponseDTO.java
@@ -4,4 +4,29 @@ public class ActiveCarePlanResponseDTO {
    public int id;
    public String status;
    public String updatedAt;
+   public ActiveCarePlanResponseDTO(int id, String status, String updatedAt) {
+    this.id = id;
+    this.status = status;
+    this.updatedAt = updatedAt;
+   }
+   public int getId() {
+      return id;
+   }
+   public void setId(int id) {
+      this.id = id;
+   }
+   public String getStatus() {
+      return status;
+   }
+   public void setStatus(String status) {
+      this.status = status;
+   }
+   public String getUpdatedAt() {
+      return updatedAt;
+   }
+   public void setUpdatedAt(String updatedAt) {
+      this.updatedAt = updatedAt;
+   }
+   
+
 }
