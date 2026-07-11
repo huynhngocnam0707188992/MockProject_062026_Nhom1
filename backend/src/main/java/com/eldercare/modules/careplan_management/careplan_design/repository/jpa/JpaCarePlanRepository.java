9d44d68746d81ba36741f1a94334d6afc4edc271
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2026-07-11 10:06:35 +0700

    modify extends interface

--- backend/src/main/java/com/eldercare/modules/careplan_management/careplan_design/repository/jpa/JpaCarePlanRepository.java
+++ backend/src/main/java/com/eldercare/modules/careplan_management/careplan_design/repository/jpa/JpaCarePlanRepository.java
@@ -1,10 +1,11 @@
 package com.eldercare.modules.careplan_management.careplan_design.repository.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.eldercare.modules.careplan_management.careplan_design.repository.database_schema.CarePlanSchema;
 
-public interface JpaCarePlanRepository extends JpaRepository<CarePlanSchema, Long>{
-    
+public interface JpaCarePlanRepository
+        extends JpaRepository<CarePlanSchema, Long>, JpaSpecificationExecutor<CarePlanSchema> {
+
 }
