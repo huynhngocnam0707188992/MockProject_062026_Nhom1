1818e54e7bd0d0bd292772f2189cd243fe78b2a5
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2026-07-17 14:39:19 +0700

    add delete one care plan dto

---
+++ backend/src/main/java/com/eldercare/modules/careplan_management/careplan_design/dto/deleteCarePlanDTO/DeleteCarePlanRequestDTO.java
@@ -0,0 +1,10 @@
+package com.eldercare.modules.careplan_management.careplan_design.dto.deleteCarePlanDTO;
+
+public class DeleteCarePlanRequestDTO {
+    public int carePlanId;
+
+    public DeleteCarePlanRequestDTO(int carePlanId) {
+        this.carePlanId = carePlanId;
+    }
+
+}
