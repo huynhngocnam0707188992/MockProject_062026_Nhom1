6d383d05c30facabf475883303696aee6d3f628e
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2026-07-17 12:14:34 +0700

    append mapper for author's care plan

--- backend/src/main/java/com/eldercare/modules/careplan_management/careplan_design/mapper/CarePlanMapper.java
+++ backend/src/main/java/com/eldercare/modules/careplan_management/careplan_design/mapper/CarePlanMapper.java
@@ -2,6 +2,7 @@ package com.eldercare.modules.careplan_management.careplan_design.mapper;
 
 import com.eldercare.common.enums.CarePlanStatusEnum;
 import com.eldercare.modules.admin.facility_setup.facility.facility_layout.entity.BedEntity;
+import com.eldercare.modules.admin.user_management.UserEntity;
 import com.eldercare.modules.careplan_management.careplan_design.entity.CareGoalEntity;
 import com.eldercare.modules.careplan_management.careplan_design.entity.CareInterventionEntity;
 import com.eldercare.modules.careplan_management.careplan_design.entity.CarePlanEntity;
@@ -10,67 +11,71 @@ import com.eldercare.modules.careplan_management.careplan_design.repository.data
 import com.eldercare.modules.resident_intake.resident_profile.ResidentEntity;
 
 public class CarePlanMapper {
-    public static CarePlanEntity toEntity(CarePlanSchema schema) {
-        if (schema == null) return null;
+        public static CarePlanEntity toEntity(CarePlanSchema schema) {
+                if (schema == null)
+                        return null;
 
-        CarePlanEntity newEntity = new CarePlanEntity();
-        newEntity.setId(schema.getId().intValue());
-        newEntity.setStatus(CarePlanStatusEnum.valueOf(schema.getStatus()));
-        BedEntity bed = schema.getResident().getBed();
-        newEntity.setResident(new CarePlanResidentInfoEntity(
-                schema.getResident().getId().intValue(),
-                schema.getResident().getLastName(),
-                schema.getResident().getDateOfBirth(),
-                bed != null && bed.getRoom() != null
-                        ? bed.getRoom().getRoomNumber()
-                        : null,
-                bed != null
-                        ? bed.getBedNumber()
-                        : null,
-                schema.getSignificantChangeFlag()
-        ));
-        newEntity.setListCareGoal(
-                schema.getListCareGoal().stream().map(careGoalschema ->
-                        new CareGoalEntity(
-                                careGoalschema.getId().intValue(),
-                                careGoalschema.getTitle(),
-                                careGoalschema.getDescription(),
-                                careGoalschema.getStatus(),
-                                careGoalschema.getListCareIntervention().stream().map(
-                                        careInterventionSchema -> new CareInterventionEntity(
-                                                careInterventionSchema.getId().intValue(),
-                                                careInterventionSchema.getTitle(),
-                                                careInterventionSchema.getAssignedRole()
-                                        )
-                                ).toList()
-                        )
-                ).toList()
-        );
-        newEntity.setCreatedAt(schema.getCreatedAt());
-        newEntity.setUpdatedAt(schema.getUpdatedAt());
-        newEntity.setIsDeleted(schema.getIsDeleted());
-        newEntity.setSignificantFlag(schema.getSignificantChangeFlag());
-        newEntity.setLastReviewDateTime(schema.getLastReviewedDateTime() == null ? null : schema.getLastReviewedDateTime());
-        newEntity.setLastReviewBy(schema.getLastReviewdBy());
+                CarePlanEntity newEntity = new CarePlanEntity();
+                newEntity.setCreatedBy(schema.getCreatedBy().getId().intValue());
+                newEntity.setId(schema.getId().intValue());
+                newEntity.setStatus(CarePlanStatusEnum.valueOf(schema.getStatus()));
+                BedEntity bed = schema.getResident().getBed();
+                newEntity.setResident(new CarePlanResidentInfoEntity(
+                                schema.getResident().getId().intValue(),
+                                schema.getResident().getLastName(),
+                                schema.getResident().getDateOfBirth(),
+                                bed != null && bed.getRoom() != null
+                                                ? bed.getRoom().getRoomNumber()
+                                                : null,
+                                bed != null
+                                                ? bed.getBedNumber()
+                                                : null,
+                                schema.getSignificantChangeFlag()));
+                newEntity.setListCareGoal(
+                                schema.getListCareGoal().stream().map(careGoalschema -> new CareGoalEntity(
+                                                careGoalschema.getId().intValue(),
+                                                careGoalschema.getTitle(),
+                                                careGoalschema.getDescription(),
+                                                careGoalschema.getStatus(),
+                                                careGoalschema.getListCareIntervention().stream().map(
+                                                                careInterventionSchema -> new CareInterventionEntity(
+                                                                                careInterventionSchema.getId()
+                                                                                                .intValue(),
+                                                                                careInterventionSchema.getTitle(),
+                                                                                careInterventionSchema
+                                                                                                .getAssignedRole()))
+                                                                .toList()))
+                                                .toList());
+                newEntity.setCreatedAt(schema.getCreatedAt());
+                newEntity.setUpdatedAt(schema.getUpdatedAt());
+                newEntity.setIsDeleted(schema.getIsDeleted());
+                newEntity.setSignificantFlag(schema.getSignificantChangeFlag());
+                newEntity.setLastReviewDateTime(
+                                schema.getLastReviewedDateTime() == null ? null : schema.getLastReviewedDateTime());
+                newEntity.setLastReviewBy(schema.getLastReviewdBy());
 
-        return newEntity;
-    }
-
-    public static CarePlanSchema toSchema(CarePlanEntity entity) {
-        if (entity == null) return null;
-
-        CarePlanSchema schema = new CarePlanSchema();
-        ResidentEntity resident = new ResidentEntity();
-        resident.setId((long)entity.getResident().getId());
-        schema.setResident(resident);
-        if (entity.getId() > 0) {
-            schema.setId((long) entity.getId());
+                return newEntity;
         }
-        schema.setStatus(entity.getStatus().name());
-        schema.setSignificantChangeFlag(entity.getSignificantFlag());
-        schema.setIsDeleted(entity.getIsDeleted());
-        schema.setCreatedAt(entity.getCreatedAt());
-        schema.setUpdatedAt(entity.getUpdatedAt());
-        return schema;
-    }
+
+        public static CarePlanSchema toSchema(CarePlanEntity entity) {
+                if (entity == null)
+                        return null;
+
+                CarePlanSchema schema = new CarePlanSchema();
+                ResidentEntity resident = new ResidentEntity();
+                resident.setId((long) entity.getResident().getId());
+                UserEntity user = new UserEntity();
+                user.setId((long) entity.getCreatedBy());
+                schema.setResident(resident);
+                if (entity.getId() > 0) {
+                        schema.setId((long) entity.getId());
+                }
+                schema.setStatus(entity.getStatus().name());
+                schema.setSignificantChangeFlag(entity.getSignificantFlag());
+                schema.setIsDeleted(entity.getIsDeleted());
+                schema.setCreatedAt(entity.getCreatedAt());
+                schema.setUpdatedAt(entity.getUpdatedAt());
+                schema.setCreatedBy(user);
+                return schema;
+        }
 }
